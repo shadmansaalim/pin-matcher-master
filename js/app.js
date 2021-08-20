29056fb20dcb7a1cc154510227ae96d3c09db439
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-08-20 19:46:58 +0600

    Fixed

--- js/app.js
+++ js/app.js
@@ -24,6 +24,15 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
         if (number == 'C') {
             calcInput.value = '';
         }
+        if (number == '<') {
+            if (calcInput.value.length == 1 || calcInput.value.length == 0) {
+                calcInput.value = '';
+            }
+            else {
+                calcInput.value = Math.floor(calcInput.value / 10);
+            }
+
+        }
     }
     else {
         const previousNumber = calcInput.value
@@ -35,23 +44,40 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
 });
 
 
-
+let count = 3;
 function verifyPin() {
     const pin = document.getElementById('display-pin').value;
     const typedNumbers = document.getElementById('typed-numbers').value;
 
-
+    console.log(pin);
     const successMessage = document.getElementById('notify-success');
 
     const failMessage = document.getElementById('notify-fail');
 
+    if (pin != '') {
+        if (pin == typedNumbers) {
+            successMessage.style.display = 'block';
+            failMessage.style.display = 'none';
+        }
+        else {
+            failMessage.style.display = 'block';
+            successMessage.style.display = 'none';
+            count -= 1;
 
-    if (pin == typedNumbers) {
-        successMessage.style.display = 'block';
-        failMessage.style.display = 'none';
+            var tryCounter = document.getElementById('try-counter');
+            tryCounter.innerText = count;
+
+
+            if (count == 0) {
+                window.location.href = 'index.html';
+            }
+
+        }
+
     }
-    else {
-        failMessage.style.display = 'block';
-        successMessage.style.display = 'none';
-    }
+
 }
+
+
+
+
